e320f570fbc0fec1d69f9b8449f6996164a72476
johnvillar41 <johndanielvillar123@gmail.com>
2022-07-16 13:23:34 +0800

    Updated Views and Controllers

--- Controllers/HomeController.vb
+++ Controllers/HomeController.vb
@@ -1,8 +1,16 @@
-﻿Public Class HomeController
+﻿Imports System.Threading.Tasks
+
+Public Class HomeController
     Inherits System.Web.Mvc.Controller
 
-    Function Index() As ActionResult
-        Return View()
+    Private _employeeService As IEmployeeService
+    Public Sub New(employeeService As IEmployeeService)
+        _employeeService = employeeService
+    End Sub
+
+    Async Function Index() As Task(Of ActionResult)
+        Dim employees = Await _employeeService.ViewAllEmployeesAsync()
+        Return View(employees)
     End Function
 
 End Class
