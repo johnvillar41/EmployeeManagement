fec899a524841c31ab747cba54dfa293bacca450
johnvillar41 <johndanielvillar123@gmail.com>
2022-07-16 12:46:08 +0800

    Added EmployeeModel

---
+++ Models/EmployeeModel.vb
@@ -0,0 +1,8 @@
+﻿Public Class EmployeeModel
+    Public Property Name As String
+    Public Property Age As Integer
+    Public Property Salary As Decimal
+    Public Property Address As String
+    Public Property BirthDate As Date
+    Public Property IsActive As Boolean
+End Class
