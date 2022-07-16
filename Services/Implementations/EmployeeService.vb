b8f515643fa9c89b9e2b7e8895b2a67baeef0841
johnvillar41 <johndanielvillar123@gmail.com>
2022-07-16 12:56:33 +0800

    Added EmployeeService

---
+++ Services/Implementations/EmployeeService.vb
@@ -0,0 +1,21 @@
+﻿Imports System.Threading.Tasks
+
+Public Class EmployeeService
+    Implements IEmployeeService
+
+    Public Function AddNewEmployeeAsync(employee As EmployeeModel) As Task Implements IEmployeeService.AddNewEmployeeAsync
+        Throw New NotImplementedException()
+    End Function
+
+    Public Function DeleteEmployeeAsync(employeeId As Integer) As Task Implements IEmployeeService.DeleteEmployeeAsync
+        Throw New NotImplementedException()
+    End Function
+
+    Public Function UpdateEmployeeAsync(employeeId As Integer, employee As EmployeeModel) As Task Implements IEmployeeService.UpdateEmployeeAsync
+        Throw New NotImplementedException()
+    End Function
+
+    Public Function ViewAllEmployeesAsync() As Task(Of IEnumerable(Of EmployeeModel)) Implements IEmployeeService.ViewAllEmployeesAsync
+        Throw New NotImplementedException()
+    End Function
+End Class
