1bf7cabeb4a4cb10d118c3a73294054fa518ecf4
johnvillar41 <johndanielvillar123@gmail.com>
2022-07-19 11:32:50 +0800

    Added Work Service

---
+++ Services/Implementations/WorkService.vb
@@ -0,0 +1,26 @@
+﻿Public Class WorkService
+    Inherits Repository(Of WorkModel)
+    Implements IWorkService
+
+    Public Overrides ReadOnly Property GetTableName As String
+        Get
+            Return "WorkTable"
+        End Get
+    End Property
+
+    Public Function AddWorkAsync(employeeId As Integer, work As WorkModel) As Object Implements IWorkService.AddWorkAsync
+        Throw New NotImplementedException()
+    End Function
+
+    Public Function DeleteWorkAsync(workId As Integer) As Object Implements IWorkService.DeleteWorkAsync
+        Throw New NotImplementedException()
+    End Function
+
+    Public Function UpdateWorkAsync(workId As Integer, work As WorkModel) As Object Implements IWorkService.UpdateWorkAsync
+        Throw New NotImplementedException()
+    End Function
+
+    Public Function FetchWorksAsync(employeeId As Integer) As Object Implements IWorkService.FetchWorksAsync
+        Throw New NotImplementedException()
+    End Function
+End Class
---
+++ Services/Interfaces/IWorkService.vb
@@ -0,0 +1,7 @@
+﻿Public Interface IWorkService
+    Function AddWorkAsync(employeeId As Integer, work As WorkModel)
+    Function DeleteWorkAsync(workId As Integer)
+    Function UpdateWorkAsync(workId As Integer, work As WorkModel)
+    Function FetchWorksAsync(employeeId As Integer)
+
+End Interface
