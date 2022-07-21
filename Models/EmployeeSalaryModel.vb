738f338cad8fc9612ae665e878ea24d9ace1c51d
johnvillar41 <johndanielvillar123@gmail.com>
2022-07-21 10:49:52 +0800

    Added SalaryModel and EmployeeSalaryModel

---
+++ Models/EmployeeSalaryModel.vb
@@ -0,0 +1,10 @@
+﻿Public Class EmployeeSalaryModel
+    Public Property Id As Integer
+    Public Property PayrollId As Integer
+    Public Property EmployeeId As Integer
+    Public Property NumberOfAbsent As Integer
+    Public Property NumberOfLate As Integer
+    Public Property Allowance As Decimal
+    Public Property Deductions As Decimal
+    Public Property Net As Decimal
+End Class
---
+++ Models/SalaryModel.vb
@@ -0,0 +1,7 @@
+﻿Public Class SalaryModel
+    Public Property Id As Integer
+    Public Property DateFrom As Date
+    Public Property DateTo As Date
+    Public Property Status As String
+    Public Property DateCreated As Date
+End Class
